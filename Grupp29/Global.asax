38cea54256e1636c2f3b07875cbe190dc395582e
emmawall <56914343+emmawall@users.noreply.github.com>
2021-03-18 09:01:38 +0100

    Add project files.

---
+++ Grupp29/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Grupp29.MvcApplication" Language="C#" %>
---
+++ Grupp29/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grupp29.Startup))]
+namespace Grupp29
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
