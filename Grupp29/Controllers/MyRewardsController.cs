fef171fb72ba2abe7f7c1329a966f675a466cb08
agnesskogfors <56914342+agnesskogfors@users.noreply.github.com>
2021-07-21 12:41:09 +0200

    lagt in så vi kan ha flera badges i mina vinster

--- Grupp29/Controllers/MyRewardsController.cs
+++ Grupp29/Controllers/MyRewardsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Grupp29.Models;
 
 namespace Grupp29.Controllers
 {
@@ -12,6 +13,44 @@ namespace Grupp29.Controllers
         public ActionResult MyRewards()
         {
             return View();
+        }
+
+        public List<Forum> GetForumPostRewardFromAuthor(string author)
+        {
+
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+            var listOfMatchingForumPosts = new List<Forum>();
+            var listOfAllForumPosts = dbContext.Fora.ToList();
+
+            foreach (Forum forumPost in listOfAllForumPosts)
+            {
+                if (forumPost.Creator.Equals(author))
+                {
+                    listOfMatchingForumPosts.Add(forumPost);
+                }
+
+            }
+            return listOfMatchingForumPosts;
+
+        }
+
+        public List<ForumPostComment> GetCommentRewardFromAuthor(string author)
+        {
+
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+            var listOfMatchingComments = new List<ForumPostComment>();
+            var listOfAllComments = dbContext.ForumPostComments.ToList();
+
+            foreach (ForumPostComment comment in listOfAllComments)
+            {
+                if (comment.Author.Equals(author))
+                {
+                    listOfMatchingComments.Add(comment);
+                }
+
+            }
+            return listOfMatchingComments;
+
         }
     }
 }
