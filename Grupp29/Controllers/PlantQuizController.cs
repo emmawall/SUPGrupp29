c4e425c8dbde1f6251551eeb8f6399666735fcb3
emmawall <56914343+emmawall@users.noreply.github.com>
2021-07-21 16:28:21 +0200

    Fixat quiz, lagt till bild på temadagar, nya växter och nya kategorier på forumet.

---
+++ Grupp29/Controllers/PlantQuizController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grupp29.Controllers
+{
+    public class PlantQuizController : Controller
+    {
+        // GET: PlantQuiz
+        public ActionResult Quiz()
+        {
+            return View();
+        }
+    }
+}
